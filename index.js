06b7e4043d79ad85c654d2209b8137273c5f58c9
Simon Balázs <balazs.simon@intren.hu>
2025-05-27 10:34:06 +0200

    Removes update availability notifications
    Eliminates dialog prompts for available updates and downloaded updates. Streamlines the update process by reducing interruptions.
    
    Addresses potential user experience issues with redundant notifications.

--- index.js
+++ index.js
@@ -192,30 +192,6 @@ app.on('will-finish-launching', () => {
     });
 });
 
-autoUpdater.on('update-available', () => {
-    dialog.showMessageBox({
-        type: 'info',
-        title: 'Frissítés elérhető',
-        message: 'Egy új verzió elérhető. Letöltés folyamatban...',
-    });
-});
-
-autoUpdater.on('update-downloaded', () => {
-    dialog
-        .showMessageBox({
-            type: 'question',
-            buttons: ['Újraindítás most', 'Később'],
-            defaultId: 0,
-            title: 'Frissítés készen áll',
-            message: 'A frissítés letöltődött. Újraindítod most?',
-        })
-        .then((result) => {
-            if (result.response === 0) {
-                autoUpdater.quitAndInstall();
-            }
-        });
-});
-
 autoUpdater.on('error', (err) => {
     console.error('Updater hiba:', err);
 });
